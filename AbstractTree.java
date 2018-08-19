19cd3126186822ff138532f7fe0df3a99fd9543b
likuncheng <1453876715@qq.com>
2018-08-19 23:01:22 +0800

    添加binaryTree，AVLTree

---
+++ AbstractTree.java
@@ -0,0 +1,16 @@
+package tree.imooc;
+
+public abstract class AbstractTree<E> implements Tree<E> {
+public void inorder(){
+	
+}
+public void preorder(){
+	
+}
+public void postorder(){
+	
+}
+public boolean isEmpty(){
+	return getSize()==0;
+}
+}
---
+++ Tree.java
@@ -0,0 +1,12 @@
+package tree.imooc;
+
+public interface Tree<E> extends Iterable<E> {
+public boolean search(E e);
+public boolean insert(E e);
+public boolean delete(E e);
+public void inorder();
+public void postorder();
+public void preorder();
+public int getSize();
+public boolean isEmpty();
+}
